3a2f674ead9a8e0655ff3f167531435240916e23
Sakina Zilat <sakina.zilat@adviqo.com>
2017-06-01 15:49:18 +0200

    remove unused imports

--- src/main/java/com/myexperience/Application.java
+++ src/main/java/com/myexperience/Application.java
@@ -2,9 +2,6 @@ package com.myexperience;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 
 /**
  * Created by zilat on 31.05.2017.
